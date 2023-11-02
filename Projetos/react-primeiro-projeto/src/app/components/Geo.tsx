2ab6303fb7a6dcfd597d05902b0d63d42797fad5
Rafael Pereira <rafaelpersan@gmail.com>
2023-11-02 11:10:18 -0300

    react com next

---
+++ Projetos/react-primeiro-projeto/src/app/components/Geo.tsx
@@ -0,0 +1,15 @@
+export const Square = () =>{
+    return (
+      <div className="w-52 h-52 bg-orange-700 text-white">
+        Texto qualquer.
+      </div>  
+    );
+}
+
+export const Circle = () =>{
+  return (
+    <div className="w-52 h-52 bg-orange-700 text-white rounded-full">
+      Texto qualquer.
+    </div>  
+  );
+}
---
+++ Projetos/react-primeiro-projeto/src/app/page.tsx
@@ -0,0 +1,15 @@
+import {Square, Circle} from './components/Geo';
+
+const Page = () =>{
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">Olá, Mundo</h1>
+      <h3>Olá</h3>
+
+      <Square />
+      <Circle />
+    </div>
+  );
+}
+
+export default Page;
